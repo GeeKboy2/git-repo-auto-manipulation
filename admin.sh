4578fc97e01818877fd8a901be1173dbe59692b9
GeeKboyboss <Mohamed.Kherraz@bordeaux-inp.fr>
2023-05-30 16:13:16 +0200

    adding admin.sh

---
+++ admin.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/bash
+
+############################################################
+# Help                                                     #
+############################################################
+help()
+{
+   echo
+   echo "-h     Call help function"
+   echo "-n     Change user.name to GeeKboyboss"
+   echo "-g     Clean git history"
+   echo
+}
+
+############################################################
+############################################################
+# Main program                                             #
+############################################################
+############################################################
+
+
+############################################################
+# Process the input options. Add options as needed.        #
+############################################################
+# Get the options
+while getopts ":h|g|n" option; do
+   case $option in
+      h)
+         help
+         ;;
+      g)
+         echo "admin.sh" >> .gitignore ; 
+         ignored=$(cat .gitignore | uniq -u); 
+         echo "$ignored" > .gitignore
+
+         git checkout --orphan TEMP_BRANCH ; git add -A ; git rm --cached ./admin.sh ; git commit -m "Initial commit" ; git branch -D main ; git branch -m main ; git push --set-upstream  -f origin main ; cp ~/Desktop/projects/git-repo-auto-manipulation/admin.sh .
+         echo "## == Successfully Cleaned Git Commit History == ##"
+         echo
+         ;;
+      n)
+         git config user.name GeeKboyboss
+         echo
+         echo "## == Successfully changed the name of user.name == ##"
+         echo
+         ;;
+   esac
+done
+
+help
+exit 0
